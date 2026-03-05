676a787001bf63c317c07378bcbf7a71cdfa1748
sh8941 <zshamoonhaider@gmail.com>
2026-03-05 19:30:11 +0530

    initial structure added

---
+++ src/main/java/com/haider/SecureTaskManagement/SecureTaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.haider.SecureTaskManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureTaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureTaskManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haider/SecureTaskManagement/SecureTaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haider.SecureTaskManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureTaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
